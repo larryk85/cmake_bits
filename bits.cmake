5089f3b6d9feb1b123003d64e26766bd0f81c54a
Bucky Kittinger <larrykittinger@gmail.com>
2020-10-22 00:39:41 -0400

    update

--- bits.cmake
+++ bits.cmake
@@ -1,2 +1,16 @@
 include(ProcessorCount)
+
 add_subdirectory(external_project)
+
+macro(add_project_dependency name)
+   set(oneValueArgs VERSION)
+   cmake_parse_arguments(ADD_DEP "${oneValueArgs}" "${ARGN}")
+
+   find_package(${name} ${ADD_DEP_VERSION} QUIET)
+
+   if(NOT ${name}_FOUND)
+      extern_project(${name}
+         VERSION ${ADD_DEP_VERSION}
+         ${ARGN})
+   endif()
+endmacro()
