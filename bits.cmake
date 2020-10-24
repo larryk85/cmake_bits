808099f5e208f2367260900ffcfd21dfe217d33d
Bucky Kittinger <larrykittinger@gmail.com>
2020-10-24 02:56:03 -0400

    added wget utils

--- bits.cmake
+++ bits.cmake
@@ -1,5 +1,7 @@
 include(ProcessorCount)
 
-add_subdirectory(external_project)
+add_subdirectory(external_project_utils)
 add_subdirectory(git_utils)
+#add_subdirectory(package_utils)
 add_subdirectory(version_utils)
+add_subdirectory(wget_utils)
