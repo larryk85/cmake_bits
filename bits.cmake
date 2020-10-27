b857aef9c2e0695e728ebfce7544bcbc7a4bfdad
Bucky Kittinger <larrykittinger@gmail.com>
2020-10-26 23:53:38 -0400

    update

--- bits.cmake
+++ bits.cmake
@@ -4,5 +4,5 @@ add_subdirectory(external_project_utils)
 add_subdirectory(git_utils)
 add_subdirectory(wget_utils) #required to be included before package_utils
 add_subdirectory(system_utils)
-#add_subdirectory(package_utils)
+add_subdirectory(package_utils)
 add_subdirectory(version_utils)
