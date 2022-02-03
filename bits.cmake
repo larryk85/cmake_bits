aaebe8363d85e768832c07f29b510b0910b4d3ca
larryk85 <larry.kittinger@bullish.com>
2022-02-03 13:11:46 -0500

    remove package/system/wget for now

--- bits.cmake
+++ bits.cmake
@@ -1,8 +1,7 @@
 include(ProcessorCount)
 
 add_subdirectory(external_project_utils)
+add_subdirectory(dependency_utils)
 add_subdirectory(git_utils)
-add_subdirectory(wget_utils) #required to be included before package_utils
 add_subdirectory(system_utils)
-add_subdirectory(package_utils)
 add_subdirectory(version_utils)
